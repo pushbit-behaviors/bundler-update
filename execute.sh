4339cd64226d70b56ebf80a962c22d9654a12f07
Tom Moor <tom.moor@gmail.com>
2016-02-10 10:14:11 -0800

    Changes to support no-clone

--- execute.sh
+++ execute.sh
@@ -8,56 +8,41 @@ rvm list
 echo known ruby versions
 rvm list known
 
-echo "Setting git defaults"
-git config --global user.email "bot@pushbit.co"
-git config --global user.name "Pushbit"
-git config --global push.default simple
+echo "Entering code directory"
+cd code
 
-echo "cloning git repo"
-git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git target
-
-echo "entering git repo"
-cd target
-
-echo "Checkout correct branch: ${BASE_BRANCH}"
-git checkout ${BASE_BRANCH}
-
-echo "cache current Gemfile.lock"
+echo "Caching Gemfile.lock"
 cp Gemfile.lock Gemfile.lock.old
 if [ $? -eq 0 ]
 then
   echo "Gemfile.lock cached"
 else
-  echo "No Gemfile.lock"
+  echo "No Gemfile.lock found"
   exit 0
 fi
 
-echo "using ruby version"
+echo "Using ruby version"
 ruby --version
 
-echo "ensure bundler"
+echo "Installing required gems"
 gem install bundler
 gem install faraday
 
-
-echo "checking out new branch"
+echo "Checking out new branch"
 git checkout -B pushbit/bundler-update
 
 export COMMIT="$(git rev-parse HEAD)"
 
-echo "manipulating source code"
+echo "Updating bundle"
 bundle update
 
 git add Gemfile.lock
 
 if git diff-index --quiet HEAD --; then
-  echo "bundle was not updated"
+  echo "Bundle was not updated"
 else
-  echo "bundle was updated"
-  echo "commiting"
+  echo "Bundle was updated"
   git commit -m "bundle updated"
-
-  echo "pushing source code"
   git push -f origin pushbit/bundler-update
 
   ruby ../execute.rb
